35a09919b10dd87dd98087c178f2507ce8020949
gowtham T <gowthamt@gowthams-MacBook-Pro.local>
2025-05-15 20:54:42 +0530

    backend is generally created with basic stuffs

--- Backend /server.js
+++ Backend /server.js
@@ -12,5 +12,5 @@ connectDB();
 app.use("/auth", require("./routes/auth.routes"));
 app.use("/users", require("./routes/user.routes"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
